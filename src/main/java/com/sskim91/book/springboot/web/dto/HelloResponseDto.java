108042f7fd2dd0767a0320b71878d3d1b46ae289
sskim <tjdtjq91@gmail.com>
2020-05-03 15:21:56 +0900

    gradle version 4.10.2 로 변경 테스트 코드 작성 방법 Lombok 추가

---
+++ src/main/java/com/sskim91/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.sskim91.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor    //선언된 모든 final 필드가 포함된 생성자를 생성한다. final이 없는 필드는 생성자에 포함되지 않는다.
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
